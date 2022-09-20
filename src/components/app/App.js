8fa39630b55567e89dbe3ac136d19a23d2fef0da
wmedders21 <wmedders@gmail.com>
2022-09-20 15:18:33 -0400

    rendered h1 and h2 elements

---
+++ src/components/app/App.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import Ideas from './../ideas/Ideas';
+import './App.css';
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      ideas: [
+        { id: 1, title: 'Prank Travis', description: 'Stick googly eyes on all his stuff' },
+        { id: 2, title: 'Make a secret password app', description: 'So you and your rideshare driver can both know neither one of you is lying' },
+        { id: 3, title: 'Learn a martial art', description: 'To exact vengeance upon my enemies' }
+      ]
+    }
+  }
+
+  render() {
+    return (
+      <main className='App'>
+        <h1>IdeaBox</h1>
+        <Ideas />
+      </main>
+    )
+  }
+}
+export default App;
---
+++ src/components/ideas/Ideas.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './Ideas.css';
+
+const Ideas = () => {
+  return (
+    <h2>Ideas go here!</h2>
+  )
+}
+
+export default Ideas;
