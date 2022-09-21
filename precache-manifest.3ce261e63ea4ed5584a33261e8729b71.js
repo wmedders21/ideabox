a4a9fe2a55e0ca69387707b512b6dd2a03b9e1ef
wmedders21 <wmedders@gmail.com>
2022-09-21 17:27:22 -0400

    Updates

---
+++ precache-manifest.3ce261e63ea4ed5584a33261e8729b71.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "d46bbe5e49a64374c7d3",
+    "url": "/ideabox/static/css/main.a647685b.chunk.css"
+  },
+  {
+    "revision": "d46bbe5e49a64374c7d3",
+    "url": "/ideabox/static/js/main.d46bbe5e.chunk.js"
+  },
+  {
+    "revision": "8c98aab82769540efb62",
+    "url": "/ideabox/static/js/1.8c98aab8.chunk.js"
+  },
+  {
+    "revision": "0d26c685d8ed13e7eac0",
+    "url": "/ideabox/static/js/2.0d26c685.chunk.js"
+  },
+  {
+    "revision": "569c53a2d4841a985daa",
+    "url": "/ideabox/static/js/runtime~main.569c53a2.js"
+  },
+  {
+    "revision": "9bd3db47717531296ad56b5bbb851925",
+    "url": "/ideabox/index.html"
+  }
+];
